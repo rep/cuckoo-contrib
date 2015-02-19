d8ca4267752f4cdad6eb2a5269163a678b1760dd
rep <ms@mwcollect.org>
2015-02-19 13:14:37 +0100

    remove rules before adding, add 53 pass-through

--- netsink/iptables.sh
+++ netsink/iptables.sh
@@ -1,6 +1,9 @@
-iptables -t nat -A PREROUTING -i vboxnet0 -p tcp -d 192.168.56.1 --dport 2042 -j ACCEPT
-iptables -t nat -A PREROUTING -i vboxnet0 -p tcp -j DNAT --to-destination 192.168.56.1:1
+for m in {D,A}; do
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p tcp -d 192.168.56.1 --dport 2042 -j ACCEPT
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p tcp -j DNAT --to-destination 192.168.56.1:1
 
-iptables -t nat -A PREROUTING -i vboxnet0 -p udp -d 255.255.255.255 -j ACCEPT
-iptables -t nat -A PREROUTING -i vboxnet0 -p udp -d 192.168.56.1 --dport 67 -j ACCEPT
-iptables -t nat -A PREROUTING -i vboxnet0 -p udp -j DNAT --to-destination 192.168.56.1:1
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -d 255.255.255.255 -j ACCEPT
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -d 192.168.56.1 --dport 67 -j ACCEPT
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -d 192.168.56.1 --dport 53 -j ACCEPT
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -j DNAT --to-destination 192.168.56.1:1
+done
