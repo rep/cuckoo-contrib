39705576c395ca810f46454c002ab61b190275cf
rep <ms@mwcollect.org>
2015-05-08 13:30:46 +0200

    upd netsink

--- netsink/iptables.sh
+++ netsink/iptables.sh
@@ -1,9 +1,19 @@
-for m in {D,A}; do
-	iptables -t nat -${m} PREROUTING -i vboxnet0 -p tcp -d 192.168.56.1 --dport 2042 -j ACCEPT
-	iptables -t nat -${m} PREROUTING -i vboxnet0 -p tcp -j DNAT --to-destination 192.168.56.1:1
+#!/bin/bash
 
-	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -d 255.255.255.255 -j ACCEPT
-	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -d 192.168.56.1 --dport 67 -j ACCEPT
-	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -d 192.168.56.1 --dport 53 -j ACCEPT
+for m in {D,A}; do
+  for iface in {vboxnet0,qemubr}; do
+	iptables -t nat -${m} PREROUTING -i $iface -p tcp -d 192.168.0.0/16 --dport 8019 -j ACCEPT
+#	iptables -t nat -${m} PREROUTING -i $iface -p tcp -d 192.168.0.0/16 --dport 2042 -j ACCEPT
+
+	iptables -t nat -${m} PREROUTING -i $iface -p udp -d 255.255.255.255 -j ACCEPT
+	iptables -t nat -${m} PREROUTING -i $iface -p udp -d 192.168.0.0/16 --dport 67 -j ACCEPT
+	iptables -t nat -${m} PREROUTING -i $iface -p udp -d 192.168.0.0/16 --dport 53 -j ACCEPT
+  done
+
 	iptables -t nat -${m} PREROUTING -i vboxnet0 -p udp -j DNAT --to-destination 192.168.56.1:1
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p icmp -j DNAT --to-destination 192.168.56.1
+	iptables -t nat -${m} PREROUTING -i vboxnet0 -p tcp -j DNAT --to-destination 192.168.56.1:1
+	iptables -t nat -${m} PREROUTING -i qemubr -p udp -j DNAT --to-destination 192.168.55.1:1
+	iptables -t nat -${m} PREROUTING -i qemubr -p icmp -j DNAT --to-destination 192.168.55.1
+	iptables -t nat -${m} PREROUTING -i qemubr -p tcp -j DNAT --to-destination 192.168.55.1:1
 done
--- netsink/netsink.py
+++ netsink/netsink.py
@@ -10,6 +10,8 @@ import threading
 
 log = logging.getLogger(__name__)
 
+CACHESIZE = 1000
+
 # random ip within 10.0.0.0/8
 def randip():
     return socket.inet_ntoa("\x0a"+os.urandom(3))
@@ -19,10 +21,15 @@ def bin2mac(x):
 
 def dns_serv(args):
     from scapy.all import DNS, DNSRR, DNSQR
+    import time
 
     udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udps.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     udps.bind((args.bind, 53))
+
+    cache = {}
+    lru = []
+    last_cleanup = time.time()
     
     while 1:
         data, addr = udps.recvfrom(1024)
@@ -31,19 +38,40 @@ def dns_serv(args):
         rp = DNS(id=p.id, qr=1, qdcount=p.qdcount, ancount=1, rcode=0)
         rp.qd = p[DNSQR]
 
-        if p.opcode == 0:
-            if p.qd[0].qname == "dns.msftncsi.com.":
-                answer_ip = "131.107.255.255"
+        # IN A
+        if p.opcode == 0 and p[DNSQR].qtype == 1 and p[DNSQR].qclass == 1:
+            if not p.qd[0].qname in cache:
+                if p.qd[0].qname == "dns.msftncsi.com.":
+                    answer_ip = "131.107.255.255"
+                else:
+                    answer_ip = randip()
+                rp.an = DNSRR(rrname=p.qd[0].qname, ttl=60, rdlen=4, rdata=answer_ip)
             else:
-                answer_ip = randip()
-            rp.an = DNSRR(rrname=p.qd[0].qname, ttl=60, rdlen=4, rdata=answer_ip)
-            log.debug("Responding to {0} with {1}.".format(p.qd[0].qname, answer_ip))
+                lru.remove(p.qd[0].qname)
+                rp.an = DNSRR(rrname=p.qd[0].qname, ttl=60, rdlen=4, rdata=cache[p.qd[0].qname])
+
+            log.debug("Responding to {0}/{2},{3} with {1}/{4},{5}.".format(p.qd[0].qname, answer_ip, p[DNSQR].qtype, p[DNSQR].qclass, rp.an.type, rp.an.rclass))
+            cache[p.qd[0].qname] = answer_ip
+            lru.append(p.qd[0].qname)
+
+        # IN PTR, just send NXDOMAIN
+        elif p.opcode == 0 and p[DNSQR].qtype == 12 and p[DNSQR].qclass == 1:
+            rp.ancount = 0
+            rp.rcode = 3
+            log.info("PTR query to {0}/{1},{2} answering nxdomain".format(p.qd[0].qname, p[DNSQR].qtype, p[DNSQR].qclass))
         else:
             rp.ancount = 0
             rp.rcode = 2
-            log.warn("Query opcode {0} for {1}, answering servfail.".format(p.opcode, p.qd[0].qname))
+            log.warn("Unhandled query opcode {0} for {1}/{2},{3} - answering servfail.".format(p.opcode, p.qd[0].qname, p[DNSQR].qtype, p[DNSQR].qclass))
 
         udps.sendto(rp.build(), addr)
+
+        if last_cleanup < time.time() - 60:
+            last_cleanup = time.time()
+            if len(cache) > CACHESIZE:
+                to_remove, lru = lru[:CACHESIZE*0.1], lru[CACHESIZE*0.1:]
+                for i in to_remove:
+                    del cache[i]
 
 def open_port(addr, port, proto="tcp"):
     try:
@@ -117,13 +145,13 @@ def port_sink(args):
                 except:
                     closesock(fd, sock)
                 else:
-                    log.debug("RECV %s:%u -> %s:%u: %s", sip, sport, dip, dport, repr(data))
+                    log.debug("RECV TCP %s:%u -> %s:%u: %s", sip, sport, dip, dport, repr(data))
                     if not data:
                         closesock(fd, sock)
                     elif "/ncsi.txt HTTP/1." in data:
                         sock.sendall("""HTTP/1.1 200 OK\r
 Content-Length: 14\r
-Content-Type: text/plain
+Content-Type: text/plain\r
 Cache-Control: max-age=30, must-revalidate\r
 \r
 Microsoft NCSI""")
@@ -140,7 +168,6 @@ def launchthread(fn, *args):
 
 def main():
     import argparse
-    import Queue
 
     logging.basicConfig(level=logging.DEBUG)
 
